ae52b83c40544faa9f226ecde7ec001e06f37540
Gabriel Seralha Miró <gsmiro@gmail.com>
2016-04-13 18:58:14 -0300

    Added sass and gulp

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+ 
+gulp.task('styles', function(){
+  return gulp.src('css/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+});
