1aa21ef335c067b90eb04b24f202f83e669e4a9a
Gabriel Seralha Miró <gsmiro@gmail.com>
2016-04-15 08:18:15 -0300

    Polymer first try

--- gulpfile.js
+++ gulpfile.js
@@ -6,5 +6,10 @@ let sass = require('gulp-sass');
 gulp.task('styles', function(){
   return gulp.src('css/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('css')) 
+});
+gulp.task('polymer', function(){
+  return gulp.src('polymer/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('polymer')) 
 });
